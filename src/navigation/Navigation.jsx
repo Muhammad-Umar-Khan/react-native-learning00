780c6c3ad52a128c789eac3d2febca0851dd5bfe
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2023-02-27 11:53:47 +0500

    initial commit

---
+++ src/navigation/Navigation.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <View>
+      <Text>This is the Navigations file</Text>
+    </View>
+  )
+}
+
+export default Navigation
+
+const styles = StyleSheet.create({})
